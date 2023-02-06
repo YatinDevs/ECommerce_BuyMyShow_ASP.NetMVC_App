44ff73522876f94198de2fa2710ec01968278d4d
YatinDevs <c.yatin727@gmail.com>
2023-02-06 20:16:08 +0530

    Add project files.

---
+++ ECommerce_BuyMyShow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerce_BuyMyShow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
